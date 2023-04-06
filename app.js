ae5d522f05353bf418a7b3912ebb62122eaf57da
Derek <derekdanielson84@gmail.com>
2023-04-06 10:28:35 -0600

    Notes added

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+const doubledNumbers = numbers.map((number) => number * 2);
 
+console.log(doubledNumbers);
 
+// Same with forEach
+const doubledNumbers2 = [];
 
+numbers.forEach((number) => {
+    doubledNumbers2.push(number * 2);
+});
 
-
-
-
-
+console.log(doubledNumbers2);
 
 
 const companies = [
@@ -21,3 +25,86 @@ const companies = [
     { name: 'Company Eight', category: 'Tech', start: 2011, end: 2016 },
     { name: 'Company Nine', category: 'Retail', start: 1981, end: 1989 }
 ];
+
+// Create an array of company names
+const companyNames = companies.map((company) => company.name);
+console.log(companyNames);
+
+
+// Create an array with just company and category
+const companyInfo = companies.map((company) => {
+    return {
+        name: company.name,
+        category: company.category
+    }
+});
+
+console.log(companyInfo);
+
+
+// Create an array of objects with the name and the length of each company in years
+const companyYears = companies.map((company) => {
+   return{
+    name: company.name,
+    length: company.end - company.start
+   }
+});
+
+console.log(companyYears);
+
+// Chain map methods
+const squareAndDouble = numbers
+    .map((number) => Math.sqrt(number))
+    .map((sqrt) => sqrt * 2);
+
+const squareAndDouble2 = numbers
+    .map(function (number){
+        return Math.sqrt(number);
+    })
+    .map(function (sqrt){
+        return sqrt * 2;
+    })
+    .map(function (sqrtDoubled){
+        return sqrtDoubled * 3;
+    });
+
+console.log(squareAndDouble);
+console.log(squareAndDouble2)
+
+
+// Chaining different methods
+const evenDouble = numbers
+    .filter((number) => number % 2 === 0)
+    .map((number) => number * 2);
+
+console.log(evenDouble);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
